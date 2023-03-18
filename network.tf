46712d0e4bd2c44c2929d005a8c9590dc8ed6421
Wahid Ramadhan <wahidramadhan791@gmail.com>
2023-03-18 17:37:06 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,3 +1,8 @@
+resource "azurerm_resource_group" "example" {
+  name     = "my-resources"
+  location = "southeastasia"
+}
+
 module "network" {
   source  = "app.terraform.io/wahid-azure/network/azurerm"
   version = "3.5.0"
